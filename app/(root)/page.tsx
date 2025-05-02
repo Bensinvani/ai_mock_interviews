6337a6e07fe3e6dc1390304ff5974e1f51b9bfb9
Ben sinvani <Bensinvani7@gmail.com>
2025-05-02 18:54:03 +0300

    implment interview session

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,10 @@
 import InterviewCard from "@/components/InterviewCard";
 import { Button } from "@/components/ui/button";
-import { dummyInterviews } from "@/constants";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 import {
-  getCurrentUser,
   getInterviewsByUserId,
   getLatestInterviews,
-} from "@/lib/actions/auth.action";
+} from "@/lib/actions/general.action";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -14,8 +13,8 @@ const page = async () => {
   const user = await getCurrentUser();
 
   const [userInterviews, latestInterviews] = await Promise.all([
-    await getInterviewsByUserId(user?.id!),
-    await getLatestInterviews({ userId: user?.id! }),
+    await getInterviewsByUserId(user?.id as string),
+    await getLatestInterviews({ userId: user?.id as string }),
   ]);
 
   const hasPastInterviews = userInterviews?.length > 0;
